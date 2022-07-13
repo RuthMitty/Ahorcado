2622711d2bcaf6a25e615844830996410293ce5f
JaviZamudio <javier.elihu100@gmail.com>
2022-07-13 18:45:56 -0500

    listo para empezar

--- src/components/Letter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Letter = () => {
-    return (
-        <div>letter</div>
-    );
-}
--- src/components/Word.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Word = () => {
-    return(
-        <div>Word</div>
-    );
-}
